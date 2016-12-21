7e9d4537c688765451f8b551814f1fa881174b64
IllusiveJack <kill1234@abv.bg>
2016-12-22 00:48:56 +0200

    Created a new program

---
+++ SecondTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SecondTestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 3;
+            int b = 4;
+            int c = 5;
+
+            int sum = a + b + c;
+
+            Console.WriteLine("The answer is " + sum);
+        }
+    }
+}
