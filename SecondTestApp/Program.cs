25d451dc1bfc0e407e4c6709f4d78d70ac0eba07
IllusiveJack <kill1234@abv.bg>
2016-12-25 02:17:59 +0200

    Wrote a homework

--- SecondTestApp/Program.cs
+++ SecondTestApp/Program.cs
@@ -5,7 +5,17 @@ class hoRRiblEcoDe
     static void Main()
     {
         {
-            Console.WriteLine(DateTime.Now);
+            int a = 1;
+           for(int i = 2; i < 1000; i++)
+            {
+                a += 1;
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine(-a);
+                }
+                else
+                    Console.WriteLine(a);
+            }
 
         }
     }
